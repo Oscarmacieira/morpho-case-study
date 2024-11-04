aa3cc03f160cf48e89b4f31cf4076e91b20bcbf2
Oscarmacieira <macieiraoscar@icloud.com>
2024-11-04 22:55:44 +0000

    feat(rainbowkit.config.ts): added okx, rabby and metamask wallets

--- lib/rainbowkit.config.ts
+++ lib/rainbowkit.config.ts
@@ -1,10 +1,21 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { envs } from "./envs";
 import { tenderlyChain } from "./tenderly.config";
+import {
+  rabbyWallet,
+  metaMaskWallet,
+  okxWallet,
+} from "@rainbow-me/rainbowkit/wallets";
 
 export const rainbowkitConfig = getDefaultConfig({
   appName: "Morpho Case Study",
   projectId: envs.NEXT_PUBLIC_RAINBOWKIT_PROJECT_ID,
   chains: [tenderlyChain],
+  wallets: [
+    {
+      groupName: "Popular",
+      wallets: [metaMaskWallet, rabbyWallet, okxWallet],
+    },
+  ],
   ssr: false,
 });
